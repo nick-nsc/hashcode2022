28d44dbfb0709485cde586461b6eedfbe81f602c
Nick <87150577+nick-nsc@users.noreply.github.com>
2022-02-22 18:06:41 +0100

    added: calculate the expected score

--- PracticeRound/main.py
+++ PracticeRound/main.py
@@ -10,6 +10,7 @@ filelist_input = [
     ]
 
 counter = 0
+final_score = 0
 
 for file in filelist_input:
     counter += 1
@@ -37,17 +38,35 @@ for file in filelist_input:
         ingredients_data['like_dislike_ratio'] = ingredients_data['like']['sum'] - ingredients_data['dislike']['sum']
 
         # select ingredients based on their like_dislike_ratio
-        ingredients_selection = ingredients_data[ingredients_data.like_dislike_ratio >= 0].reset_index()
-        ingredients_selection_count = len(ingredients_selection)
+        ingredients_selected = ingredients_data[ingredients_data.like_dislike_ratio >= 0].reset_index()
+        ingredients_notselected = ingredients_data[ingredients_data.like_dislike_ratio < 0].reset_index()
+        ingredients_selected_count = len(ingredients_selected)
+
+        # calculate expected score
+        clients_selected = clients_data
+        # get every client who dislikes at least one of the selected ingredients and exclude them from the selected clients
+        for ingredient in ingredients_selected['ingredient']:
+            indizes_to_drop = clients_selected[(clients_selected['ingredient'] == ingredient) & (clients_selected['dislike'] == 1)]['client_ID']
+            clients_selected = clients_selected[~clients_selected['client_ID'].isin(indizes_to_drop)]
+            # get every client who likes at least one of the not selected ingredients and exclude them from the selected clients
+        for ingredient in ingredients_notselected['ingredient']:
+            indizes_to_drop = clients_selected[(clients_selected['ingredient'] == ingredient) & (clients_selected['like'] == 1)]['client_ID']
+            clients_selected = clients_selected[~clients_selected['client_ID'].isin(indizes_to_drop)]
+        
+        clients_selected_count = len(clients_selected.groupby('client_ID'))
+        final_score += clients_selected_count
+        print("Expected score: {}".format(clients_selected_count))
 
         # create output file
         with open(os.path.join(os.path.dirname(__file__), "output", file.replace("in.txt", "out.txt")), "w") as f:
-            solution = str(ingredients_selection_count)
-            for i in ingredients_selection['ingredient']:
+            solution = str(ingredients_selected_count)
+            for i in ingredients_selected['ingredient']:
                 solution += " "
                 solution += i
             f.write(solution)
 
     print("{} of {} finished.".format(counter, len(filelist_input)))
 
+print("-------------------------------")
+print("Final Score: {}".format(final_score))
 print("DONE")
