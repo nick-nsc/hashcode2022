618552cc82e45c9f386774195063083fe3cd6faa
Nick <87150577+nick-nsc@users.noreply.github.com>
2022-03-09 13:32:40 +0100

    c++ solution initial commit

---
+++ Problem/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void solve(const string& filename) {
+    // prepare in- and out-files
+    cout << "Processing: " << filename << "\n";
+    const string outfilename = "solution." + filename;
+
+    // set up streams
+    ifstream input;
+    ofstream output;
+    input.open(".\\..\\input_data\\"+filename, ios::in);
+    output.open(".\\..\\output\\"+outfilename, ios::out);
+    if(input.fail() || output.fail()){
+        cout << "ERROR: handling the files went wrong." << "\n";
+        exit(1);
+    }
+
+    // prepare data containers
+    map<string, map<string, int>> contributors;
+    typedef struct {
+        int days, score, bestbefore;
+        vector<pair<string, int>> roles;
+    } project;
+    
+    map<string, project> projects;
+
+    // get cont and proj count
+    int cont_count = 0, proj_count = 0;   
+    input >> cont_count >> proj_count; // cin knows that those are ints, so it reads them as such.
+
+    // get contributor data
+    for (int i = 1; i <= cont_count; ++i) {
+        // cout << "contributor " << i << "\n";
+    }
+
+    // get project data
+    for (int i = 1; i <= proj_count; ++i) {
+        // cout << "project " << i << "\n";
+    }
+
+    input.close();
+    output.close();
+}
+
+int main(int, char**) {
+    const array<string, 6> filenames {
+        "a_an_example.in.txt",
+        "b_better_start_small.in.txt",
+        "c_collaboration.in.txt",
+        "d_dense_schedule.in.txt",
+        "e_exceptional_skills.in.txt",
+        "f_find_great_mentors.in.txt"
+    };
+        
+    for (int i = 0; i < 6; i++) {
+        solve(filenames.at(i));
+    }
+        
+    cout << "DONE." << "\n";
+}
